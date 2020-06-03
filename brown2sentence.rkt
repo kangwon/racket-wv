f684b52b6aa4666ac349faca8d6bbb41440633f6
kangwon1260@gmail.com <753951zz>
2020-06-03 21:21:13 +0900

    Distill sentences from Brown corpus

---
+++ brown2sentence.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(require 2htdp/batch-io)
+
+
+(define DATA_DIR "data/brown/")
+
+
+(define cats (read-lines (string-append DATA_DIR "cats.txt")))
+
+(define (cat-filename cat)
+  (first (string-split cat)))
+
+(define filenames (map cat-filename cats))
+
+
+
+(define (string-join-\n strs)
+  (string-join strs "\n"))
+
+(define (line-words line)
+  (map string-downcase
+    (filter (lambda (token) (regexp-match? #px"\\w+" token))
+      (map (lambda (token) (first (string-split token "/"))) 
+        (string-split line)))))
+
+(write-file
+  "data/brown-sentence.txt"
+  (string-join-\n
+    (for/list ([filename filenames])
+      (string-join-\n
+        (map string-join
+          (map line-words
+            (map string-trim
+              (filter non-empty-string?
+                (read-lines (string-append DATA_DIR filename))))))))))
